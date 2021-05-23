275097e0026c5a3508e167898643b4b1f2f6ae4c
Lucas Czech <luc.czech@gmail.com>
2021-05-22 22:25:56 -0700

    Add tool run scripts

---
+++ popoolation/diversity.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# We use the same script for all measures in PoPoolation, for simplicity
+DATA=$1
+MEASURE=$2
+
+mkdir -p ${MEASURE}
+# rm ${MEASURE}/*
+
+START=$(date +%s.%N)
+echo "Start `date`"
+
+perl ../software/popoolation/Variance-sliding.pl \
+    --input ${DATA} \
+    --output "${MEASURE}/$(basename ${DATA}).${MEASURE}" \
+    --measure ${MEASURE} \
+    --fastq-type sanger \
+    --window-size 1000 \
+    --step-size 1000 \
+    --pool-size 100 \
+    --min-count 2 \
+    --min-coverage 4 \
+    --max-coverage 1000000 \
+    --min-qual 0 \
+    > ${MEASURE}/$(basename ${DATA}).log 2>&1
+
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+
+echo "End `date`"
+echo "Internal time: ${DIFF}"
---
+++ popoolation/fst.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DATA=$1
+
+mkdir -p fst
+# rm fst/*
+
+START=$(date +%s.%N)
+echo "Start `date`"
+
+perl ../software/popoolation2/fst-sliding.pl \
+    --input ${DATA} \
+    --output "fst/$(basename ${DATA}).fst" \
+    --suppress-noninformative \
+    --min-count 6 \
+    --min-coverage 50 \
+    --max-coverage 200 \
+    --min-covered-fraction 1 \
+    --window-size 1 \
+    --step-size 1 \
+    --pool-size 100
+    > fst/$(basename ${DATA}).log 2>&1
+
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+
+echo "End `date`"
+echo "Internal time: ${DIFF}"
